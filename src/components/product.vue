f5a0a819057fdefb6ceae5ea56a8f244e515665c
Petre Macamete <petreleonardos@gmail.com>
2021-05-13 03:25:54 +0300

    extract from database

--- src/components/product.vue
+++ src/components/product.vue
@@ -1,10 +1,46 @@
 <template>
-  <p>product</p>
+<div>
+  <p>product ID {{$route.query.ID}}</p>
+  <p>{{list_of_ratings}}</p>
+  <p>{{list_of_recommendations}}</p>
+</div>
 </template>
 
 <script>
+import recommendation from '../services/recommendation'
+import ratings from '../services/ratings'
 export default {
-    name:'product'
+    name:'product',
+    data:function (){
+      return{
+        list_of_ratings:[],
+        list_of_recommendations:[]
+      }
+    },
+    methods: {
+      fill : function(ID){
+        if(ID !== undefined){
+          recommendation.getRecommendationForProduct(ID).then(
+            data => {
+              this.list_of_recommendations = data.data.response
+            }
+          )
+          ratings.getRatingsFromProduct(ID).then(
+            data => {
+              this.list_of_ratings = data.data
+            }
+          )
+        }
+      }
+    },
+    mounted: function(){
+      this.fill(this.$route.query.ID)
+    },
+    watch: {
+      $route (to){
+        this.fill(to.query.ID)
+      }
+    }
 }
 </script>
 
--- src/components/user.vue
+++ src/components/user.vue
@@ -1,10 +1,46 @@
 <template>
-  <p>user</p>
+<div>
+  <p>user ID {{$route.query.ID === undefined}}</p>
+  <p>{{list_of_recommendations}}</p>
+  <p>{{list_of_ratings}}</p>
+</div>
 </template>
 
 <script>
+import recommendation from '../services/recommendation'
+import ratings from '../services/ratings'
 export default {
-    name:'user'
+    name:'user',
+    data:function (){
+      return{
+        list_of_ratings:[],
+        list_of_recommendations:[]
+      }
+    },
+    methods: {
+      fill : function(ID){
+        if(ID !== undefined){
+          recommendation.getRecommendationForUser(ID).then(
+            data => {
+              this.list_of_recommendations = data.data.response
+            }
+          )
+          ratings.getRatingsFromUser(ID).then(
+            data => {
+              this.list_of_ratings = data.data
+            }
+          )
+        }
+      }
+    },
+    mounted: function(){
+      this.fill(this.$route.query.ID)
+    },
+    watch: {
+      $route (to){
+        this.fill(to.query.ID)
+      }
+    }
 }
 </script>
 
